d445eaeed0777631924fb710d6b344a9b395b464
jengle-dev <j.engle.dev@gmail.com>
2023-01-17 21:15:41 -0600

    updated script and html, still working through localStorage and event click, may need to rearrange order of tasks within main wrapping function

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,73 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-dayjs().hour() // gets current hour
-newHour = dayjs().hour(12);
-dayjs().date()
-currentDateEl = dayjs().date();
-var containerEl = $('.container');
-// var currentHourEl = moment().hour();
-// moment().format("MMM Do YY")
-var todayEl dayjs().toDate();
+// working through TODOs in the order they appear on the screen and not the order they are presented in starter code
 
-var currentWorkHour = [
-  dayjs().format('dddd, MMMM D, YYYY'
-  )
+// Global variables
+var today = dayjs();
+var currentHour = dayjs().hour(12);
+// Array that lists all of the working hours for this current work day
+var workDayHours = [
+  dayjs().hour(9).format('hA'),
+  dayjs().hour(10).format('hA'),
+  dayjs().hour(11).format('hA'),
+  dayjs().hour(12).format('hA'),
+  dayjs().hour(13).format('hA'),
+  dayjs().hour(14).format('hA'),
+  dayjs().hour(15).format('hA'),
+  dayjs().hour(16).format('hA'),
+  dayjs().hour(17).format('hA')
 ];
+//target the div with the hour
+var timeBlockHour = $('col-md-1 hour')
+//target the div with task info
+var task = $('.description')
+// end Global Variables
 
-var timeBlock = $('col-1 hour');
-var userInput = $('.textInput');
+//wrapping function allows html page to load before running
+$(function () {
 
-var reformatDate = thisInstance().format('MMMM D YYYY');
-$('#currentDay').text(reformatDate);
+  // Time
+  $(function dateTime() {
+    $('#currentDay').text(today.format('dddd MMM D, YYYY hh:mm'));
+    console.log(today);
+  });
 
-   // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  //parse current hour & compare
+  console.log(currentHour);
+
+  function compareTimeBlock(reviewTimeBlock) {
+    var currentTimeHour = dayjs($(timeBlockHour).text().trim(), 'hA').hour();
+
+    // remove any preset class of past/present/future
+    $(reviewTimeBlock).removeClass('past present future');
+
+    if (currentTimeBlock > currentHour) {
+      $(reviewTimeBlock).addClass('future');
+    }
+    else if (currentTimeBlock === currentHour) {
+      $(reviewTimeBlock).addClass('present');
+    }
+    else {
+      $(reviewTimeBlock).addClass('past');
+    }
+  }
+
+  function textDescription() {
+    // Loop index of array of workDayHours
+    for (var i = 0; i < workDayHours.length; i++) {
+      let task = localStorage.getItem(workDayHours[i]);
+
+      //starting +9 because starting hour is 9AM and index of array begin with 0
+      if (task) {
+        $('#' + (i + 9)).siblings().first().children().text(task);
+      }
+  }
 
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  // gets current hour
-  
-  // ON CLICK - save
-  // RETREIVE current timestamp
-  // PARSE Hour
-  
-  // COMPARE Hour to option
-    // IF "past" 
-      // UPDATE CLASS - to "past"
-    // IF "present"
-      // UPDATE CLASS - to "present"
-    // IF "future"
-      // UPDATE CLASS - to "future"
+  //save to local storage upon click
+  function saveDescription(hour, task) {
+    localStorage.setItem(hour,task);
+  }
 
-  // SAVE to LOCAL STORAGE
+  for (var i = 0; i < workDayHours.length; i++) {
+   var userInput = $('<p>').text(' ').attr({id:'hour-' + (i+9)});
 
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
+  });
